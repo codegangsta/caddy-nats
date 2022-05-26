8e93fc37e8a83ef841e8254e3b178163a7609991
Jeremy Saenz <jeremy.saenz@gmail.com>
2022-05-26 11:52:30 -0700

    Added index placeholders for nats subjects

--- replacer.go
+++ replacer.go
@@ -2,6 +2,7 @@ package caddynats
 
 import (
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/caddyserver/caddy/v2"
@@ -11,10 +12,34 @@ func addNATSVarsToReplacer(repl *caddy.Replacer, req *http.Request, _ http.Respo
 	natsVars := func(key string) (any, bool) {
 		if req != nil {
 			switch key {
+			// generated nats subject
 			case "nats.subject":
 				p := strings.Trim(req.URL.Path, "/")
 				return strings.ReplaceAll(p, "/", "."), true
 			}
+
+			// subject parts
+			if strings.HasPrefix(key, natsSubjectReplPrefix) {
+				idxStr := key[len(natsSubjectReplPrefix):]
+				idx, err := strconv.Atoi(idxStr)
+				if err != nil {
+					return "", false
+				}
+
+				p := strings.Trim(req.URL.Path, "/")
+				parts := strings.Split(p, "/")
+
+				if idx < 0 {
+					return "", false
+				}
+
+				if idx >= len(parts) {
+					return "", true
+				}
+
+				return parts[idx], true
+			}
+
 		}
 
 		return nil, false
@@ -22,3 +47,5 @@ func addNATSVarsToReplacer(repl *caddy.Replacer, req *http.Request, _ http.Respo
 
 	repl.Map(natsVars)
 }
+
+var natsSubjectReplPrefix = "nats.subject."
